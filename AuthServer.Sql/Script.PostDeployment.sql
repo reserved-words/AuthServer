03a49818b1278ad8ecb928d31ba7c53161dd79d5
Rhian Adams <rhian.gwawr@gmail.com>
2019-10-27 16:52:44 +0000

    Corrected app user name

--- AuthServer.Sql/Script.PostDeployment.sql
+++ AuthServer.Sql/Script.PostDeployment.sql
@@ -1,8 +1,8 @@
 ﻿
-IF NOT EXISTS (SELECT LoginName FROM SYSLOGINS WHERE NAME = 'auth-app')
+IF NOT EXISTS (SELECT LoginName FROM SYSLOGINS WHERE NAME = 'test-vm\auth-app')
 BEGIN
 	BEGIN TRY  
-		CREATE LOGIN [auth-app] FROM WINDOWS WITH DEFAULT_DATABASE = [Auth], DEFAULT_LANGUAGE = [us_english]
+		CREATE LOGIN [test-vm\auth-app] FROM WINDOWS WITH DEFAULT_DATABASE = [Auth], DEFAULT_LANGUAGE = [us_english]
 	END TRY  
 	BEGIN CATCH  
 		-- OK - if this fails we're in dev environment  
@@ -10,23 +10,23 @@ BEGIN
 END
 GO
 
-IF EXISTS (SELECT LoginName FROM SYSLOGINS WHERE NAME = 'auth-app')
+IF EXISTS (SELECT LoginName FROM SYSLOGINS WHERE NAME = 'test-vm\auth-app')
 BEGIN
 
-	IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = 'auth-app')
+	IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = 'test-vm\auth-app')
 	BEGIN
-		CREATE USER [auth-app] FOR LOGIN [auth-app] WITH DEFAULT_SCHEMA = dbo
+		CREATE USER [test-vm\auth-app] FOR LOGIN [test-vm\auth-app] WITH DEFAULT_SCHEMA = dbo
 	END
 
-	GRANT CONNECT TO [auth-app]
+	GRANT CONNECT TO [test-vm\auth-app]
 
-	GRANT EXECUTE ON [dbo].[FindApiResourceByName] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[FindApiResourcesByScope] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[FindClientById] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[FindProviderById] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[FindUserByExternalProvider] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[FindUserByUsername] TO [auth-app]
-	GRANT EXECUTE ON [dbo].[GetApiResources] TO [auth-app]
+	GRANT EXECUTE ON [dbo].[FindApiResourceByName] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[FindApiResourcesByScope] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[FindClientById] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[FindProviderById] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[FindUserByExternalProvider] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[FindUserByUsername] TO [test-vm\auth-app]
+	GRANT EXECUTE ON [dbo].[GetApiResources] TO [test-vm\auth-app]
 
 END
 GO
