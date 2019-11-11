1223e890755c89b3695832a81e5045b75310fde2
Rhian Adams <rhian.gwawr@gmail.com>
2019-11-11 18:40:29 +0000

    Added deployment scripts

---
+++ AppSetup.ps1
@@ -0,0 +1,11 @@
+param($DomainName, $AppName, $AppUserPassword, $CertificateThumbprint)
+
+Import-Module $PSScriptRoot\SetupTools.psm1
+Import-Module $PSScriptRoot\AddUserToCertificate.psm1
+
+$AppUserName = $DomainName + '\auth-app'
+
+Run-Setup -DomainName $DomainName
+Setup-WebApp -AppName $AppName
+New-WebUser -UserName $AppUserName -Password $AppUserPassword -AppFolderName $AppName
+Add-UserToCertificate -UserName $AppUserName -Permission read -CertStoreLocation \LocalMachine\My -CertThumbprint $CertificateThumbprint
